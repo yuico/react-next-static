051d9ab4d357bb3fe4a6c74d6bce0344e8a0e3ca
yuikim <nokdoo00@gmail.com>
2021-04-11 11:30:30 -0400

    usage of getStaticProps to fetch data when the page is built only

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import { GetStaticProps } from 'next';
 import { Microphone } from '../../model/Microphone';
+import { openDB } from '../openDB';
+import Link from 'next/link';
 
 export interface IndexProps {
   microphones: Microphone[];
 }
 
 export default function index({ microphones }: IndexProps) {
-  return <div>Test {microphones}</div>;
+  console.log(JSON.stringify(microphones));
+  return (
+    <div>
+      {microphones.map((micro) => (
+        <Link href='/[id]' as={`/${micro.id}`}>
+          <a>{micro.brand + micro.model}</a>
+        </Link>
+      ))}
+    </div>
+  );
 }
+
+//getStaticProps run only when the page is built
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const db = await openDB();
+  const microphones = await db.all('select * from microphone');
+
+  return { props: { microphones } };
+};
